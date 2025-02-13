8dfe683c60a1f5b44d80c70576f10fa62ed12db5
faisal-hajari <faisalhejary@gmail.com>
2025-02-13 18:18:03 +0300

    add kpt detection to the head

--- models/yolo.py
+++ models/yolo.py
@@ -22,22 +22,25 @@ except ImportError:
     thop = None
 
 
-class Detect(nn.Module):
+class Detect(nn.Module): #we will add the pose to this calass 
+    
     stride = None  # strides computed during build
     export = False  # onnx export
 
     def __init__(self, nc=80, anchors=(), ch=()):  # detection layer
         super(Detect, self).__init__()
         self.nc = nc  # number of classes
-        self.no = nc + 5  # number of outputs per anchor
+        self.nk = 5 
+        self.no = (nc + 5) + (self.nk*2) # number of outputs per anchor
         self.nl = len(anchors)  # number of detection layers
         self.na = len(anchors[0]) // 2  # number of anchors
         self.grid = [torch.zeros(1)] * self.nl  # init grid
         a = torch.tensor(anchors).float().view(self.nl, -1, 2)
         self.register_buffer('anchors', a)  # shape(nl,na,2)
         self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
-        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
-
+        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv  
+        
+        
     def forward(self, x):
         # x = x.copy()  # for profiling
         z = []  # inference output
@@ -45,15 +48,22 @@ class Detect(nn.Module):
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
-
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()            
+            
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
-
+                
+                # BBox 
                 y = x[i].sigmoid()
                 y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
                 y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                
+                #Keypoint 
+                kpts = y[..., 5 + self.nc:].view(bs, self.na, ny, nx, self.nk, 2)
+                kpts = (kpts * 2. - 0.5 + self.grid[i].unsqueeze(-2)) * self.stride[i]  # decode keypoints
+                y = torch.cat((y[..., :5+self.nc], kpts.view(bs, self.na, ny, nx, -1)), -1)
+                
                 z.append(y.view(bs, -1, self.no))
 
         return x if self.training else (torch.cat(z, 1), x)
@@ -63,6 +73,7 @@ class Detect(nn.Module):
         yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
         return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
 
+    
 
 class Model(nn.Module):
     def __init__(self, cfg='yolov5s.yaml', ch=3, nc=None, anchors=None):  # model, input channels, number of classes
